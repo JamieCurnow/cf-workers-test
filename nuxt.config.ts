618f7d5dc76b0fa1a43028a6f88afeee7531ff16
Jamie Curnow <jamie@island-web.ca>
2025-04-09 18:09:25 +0100

    nitro preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: "cloudflare_module",
+  },
   compatibilityDate: "2025-04-09",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/ui"],
